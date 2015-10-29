01309d6a42fa05f83e1f30de745f8e03755155a9
Y Okumura <uokumura@me.com>
2015-10-30 00:09:11 +0900

    DB初期化スクリプトを監視下に

---
+++ src/main/migrate/LocalDynamoDB.groovy
@@ -0,0 +1,22 @@
+import com.amazonaws.auth.profile.*
+import com.amazonaws.services.dynamodbv2.*
+import com.amazonaws.services.dynamodbv2.document.*
+import com.amazonaws.services.dynamodbv2.model.*
+import com.amazonaws.services.dynamodbv2.datamodeling.*
+
+import buttle7.sandbox.*
+
+def client = new AmazonDynamoDBClient(new ProfileCredentialsProvider());
+client.setEndpoint("http://localhost:8000"); 
+DynamoDB dynamoDB = new DynamoDB(client);
+
+dynamoDB.getTable("Score").delete()
+
+DynamoDBMapper mapper = new DynamoDBMapper(client)
+CreateTableRequest request = mapper.generateCreateTableRequest(Score)
+    .withProvisionedThroughput(new ProvisionedThroughput()
+        .withReadCapacityUnits(5L)
+        .withWriteCapacityUnits(6L)
+    )
+            
+dynamoDB.createTable(request).waitForActive()
